b34b80e8ee85a5f65cfa78edb3e9e2bc3dffa349
devbysoham <sohammazumder18@gmail.com>
2026-01-06 15:07:33 +0530

    new firebase app added

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -7,14 +7,15 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDBqg8GqkcY1XhA2l9y_qinQTbKFtefh5U",
-  authDomain: "ai-based-accident-detection.firebaseapp.com",
-  projectId: "ai-based-accident-detection",
-  storageBucket: "ai-based-accident-detection.firebasestorage.app",
-  messagingSenderId: "155552596068",
-  appId: "1:155552596068:web:9a8f4afb2065237ed35f71",
-  measurementId: "G-7D1QQ671HL"
+  apiKey: "AIzaSyDfLDfT0wZS7JxgQL0_D-o_kQM3xBnPxH8",
+  authDomain: "accident-detect-3.firebaseapp.com",
+  projectId: "accident-detect-3",
+  storageBucket: "accident-detect-3.firebasestorage.app",
+  messagingSenderId: "804136463716",
+  appId: "1:804136463716:web:972dfd67fdb9e9b8edde71",
+  measurementId: "G-FQCY9GYB9W"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
